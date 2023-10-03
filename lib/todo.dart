0c1774f6e09adfe19b1c669aa8db70e8819d56bc
PLWEP <langgeng86@gmail.com>
2023-10-03 22:12:02 +0700

    menambahkan method model dan class database helper

--- lib/todo.dart
+++ lib/todo.dart
@@ -10,4 +10,22 @@ class Todo {
     required this.description,
     required this.completed,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'completed': completed,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      completed: map['completed'] as bool,
+    );
+  }
 }
